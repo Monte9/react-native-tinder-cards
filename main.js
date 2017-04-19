5d01a00109e50ed3a01c76593ee86889d8fe1a42
Monte9 <Monte9@users.noreply.github.com>
2017-04-18 22:18:32 -0700

    working tinder cards with swipe animation

--- main.js
+++ main.js
@@ -1,12 +1,64 @@
 import Expo from 'expo';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class App extends React.Component {
+import { Button, Card } from 'react-native-elements'
+
+import Deck from './src/Deck'
+
+// test data
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
+
+class App extends Component {
+  renderCard(card) {
+    return (
+      <Card
+        key={card.id}
+        title={card.text}
+        image={{ uri: card.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}>I can customize the card further</Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03a9f4"
+          title="View now!"
+        />
+      </Card>
+    )
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title= "All done!">
+        <Text style={{ marginBottom: 10 }}>
+          All cards have been swiped! No more. 
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03a9f4"
+          title="Load More!"
+        />
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
       </View>
     );
   }
@@ -15,9 +67,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
 
---
+++ src/Deck.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import {
+  View, 
+  Animated, 
+  PanResponder,
+  Dimensions,
+  StyleSheet
+} from 'react-native';
+
+const SCREEN_WIDTH =  Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.4 * SCREEN_WIDTH
+
+export default class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+  
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
+        } else {
+          console.log("Swipe dismissed")
+          this.resetPosition()
+        }        
+      }
+    });
+
+    this.state = { panResponder, position, index: 0 };
+  }
+
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+
+    Animated.timing(this.state.position, {
+      toValue: { x: x * 2, y: 0 },
+      duration: 750
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeRight, onSwipeLeft, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
+    this.state.position.setValue({x: 0, y: 0})
+    this.setState({ index: this.state.index + 1})
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 2, 0, SCREEN_WIDTH * 2],
+      outputRange: ['-60deg', '0deg', '60deg']
+    })
+
+    return { 
+      ...this.state.position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+
+  renderCards() {
+    if(this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards()
+    }
+
+    return this.props.data.map((item, i) => {
+      if(i < this.state.index) {
+        return null;
+      } else if(i === this.state.index) {
+        return (
+          <Animated.View
+            style={[this.getCardStyle(), styles.cardStyle]} 
+            {...this.state.panResponder.panHandlers}
+          >
+          {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
+      return (
+        <Animated.View style={styles.cardStyle}>
+          {this.props.renderCard(item)}
+        </Animated.View>
+      )
+    }).reverse();
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
+});
