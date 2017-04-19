2625fda5508be3e2b67690f607a05b6617df6e59
Monte9 <Monte9@users.noreply.github.com>
2017-04-19 00:56:20 -0700

    added age; updated fontSize for name

--- main.js
+++ main.js
@@ -11,13 +11,13 @@ const SCREEN_HEIGHT =  Dimensions.get('window').height;
 
 // test data
 const DATA = [
-  { id: 1, text: 'Amanda', uri: 'http://f9view.com/wp-content/uploads/2013/10/American-Beautiful-Girls-Wallpapers-Hollywood-Celebs-1920x1200px.jpg' },
-  { id: 2, text: 'Emma', uri: 'https://i.imgur.com/FHxVpN4.jpg' },
-  { id: 3, text: 'Scarlett', uri: 'https://i.ytimg.com/vi/GOJZ5TIlc3M/maxresdefault.jpg' },
-  { id: 4, text: 'Keira', uri: 'http://www.bdprimeit.com/wp-content/uploads/Keira-Knightley-Most-beautiful-Hollywood-actress.jpg' },
-  { id: 5, text: 'Ashley', uri: 'https://s-media-cache-ak0.pinimg.com/736x/4c/89/67/4c8967fac1822eeddf09670565430fd5.jpg' },
-  { id: 6, text: 'Jennifer', uri: 'https://2.bp.blogspot.com/-Vy0NVWhQfKo/Ubma2Mx2YTI/AAAAAAAAH3s/LC_u8LRfm8o/s1600/aimee-teegarden-04.jpg' },
-  { id: 7, text: 'Sarah', uri: 'https://s-media-cache-ak0.pinimg.com/736x/41/75/26/4175268906d97492e4a3175eab95c0f5.jpg' },
+  { id: 1, text: 'Amanda', age: 28, uri: 'http://f9view.com/wp-content/uploads/2013/10/American-Beautiful-Girls-Wallpapers-Hollywood-Celebs-1920x1200px.jpg' },
+  { id: 2, text: 'Emma', age: 29, uri: 'https://i.imgur.com/FHxVpN4.jpg' },
+  { id: 3, text: 'Scarlett', age: 25, uri: 'https://i.ytimg.com/vi/GOJZ5TIlc3M/maxresdefault.jpg' },
+  { id: 4, text: 'Keira', age: 27, uri: 'http://www.bdprimeit.com/wp-content/uploads/Keira-Knightley-Most-beautiful-Hollywood-actress.jpg' },
+  { id: 5, text: 'Ashley', age: 30, uri: 'https://s-media-cache-ak0.pinimg.com/736x/4c/89/67/4c8967fac1822eeddf09670565430fd5.jpg' },
+  { id: 6, text: 'Jennifer', age: 24, uri: 'https://2.bp.blogspot.com/-Vy0NVWhQfKo/Ubma2Mx2YTI/AAAAAAAAH3s/LC_u8LRfm8o/s1600/aimee-teegarden-04.jpg' },
+  { id: 7, text: 'Sarah', age: 28, uri: 'https://s-media-cache-ak0.pinimg.com/736x/41/75/26/4175268906d97492e4a3175eab95c0f5.jpg' },
 ];
 
 class App extends Component {
@@ -26,8 +26,8 @@ class App extends Component {
       <Card
         key={card.id}
         containerStyle={{borderRadius: 10, width: SCREEN_WIDTH * 0.92, height: SCREEN_HEIGHT - 165}}
-        featuredTitle={card.text}
-        featuredTitleStyle={{position: 'absolute', left: 15, bottom: 10 }}
+        featuredTitle={`${card.text}, ${card.age}`}
+        featuredTitleStyle={{position: 'absolute', left: 15, bottom: 10, fontSize: 30 }}
         image={{ uri: card.uri }}
         imageStyle={{borderRadius: 10, width: SCREEN_WIDTH * 0.915, height: SCREEN_HEIGHT - 165}}
       />
